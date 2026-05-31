b6ccffc1c13005300d4bad880b0e3c6da5174c72
Vinrawa <rvinayak133@gmail.com>
2026-05-31 13:10:48 +0530

    Update metadata for v2.1.3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId   = "com.vinmusic"
         minSdk          = 26
         targetSdk       = 35
-        versionCode     = 4
-        versionName     = "2.1.2"
+        versionCode     = 5
+        versionName     = "2.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
